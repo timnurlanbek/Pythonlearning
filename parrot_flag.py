dd666b92e77585fc3664ec847a48cb5b7ae02d29
Temirbek Nurlanbek uulu <nurlanbekuulutemirbek@gmail.com>
2022-04-21 12:13:38 -0500

    parrot flag added

---
+++ parrot_flag.py
@@ -0,0 +1,11 @@
+answer = 50                             #provide answer
+guess = 49 #provide guess
+
+if guess<50:
+    result = "Oops!  Your guess was too low."
+elif guess>50:
+    result = "Oops!  Your guess was too high."
+elif guess==50:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
