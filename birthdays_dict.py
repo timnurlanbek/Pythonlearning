8f4081a416d31e02f2b2a0892a864bb42faa674b
Temirbek Nurlanbek uulu <nurlanbekuulutemirbek@gmail.com>
2022-04-22 01:34:47 -0500

    Hi gitler

---
+++ birthdays_dict.py
@@ -0,0 +1,16 @@
+brthds={
+    "apam":"21-nov",
+    "avam":"16-June",
+    "Kama akam": "23-oktober",
+    "Gulzada ejem": "25-May",
+    "Zuura":"3-March",
+    "Batma":"3-March",
+}
+
+
+# for key, value in brthds.items():
+#     print("n\Key: " + key )
+#     print("Value: " + value)
+
+# for sibling, birthday in brthds.items():
+    # print(sibling+"'s " + "birthday is "+birthday)
